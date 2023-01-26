824aceccb399eab4d5d51cd443fb5391e64b4199
ctallner@hotmail.com <ctallner@hotmail.com>
2023-01-26 09:36:29 +0100

    test

--- src/main/java/com/cte/bilar/BilarController.java
+++ src/main/java/com/cte/bilar/BilarController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class BilarController {
     @GetMapping(value = "/car")
